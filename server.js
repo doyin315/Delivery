5570953e29043ccf62abfd4451ba73049abff566
doyin315 <doyin315@gmaiil.com>
2020-01-21 21:10:40 +0100

    update to deploy to heroku

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+const path = require('path');
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
